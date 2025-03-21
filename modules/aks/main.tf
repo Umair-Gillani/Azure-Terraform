6b9a25b505c041567e7bcc79557d9dcfe9f3689c
Umair <traximuser24@gmail.com>
2025-03-22 01:38:24 +0500

    NOT TEST ONLY 'PRIVATE CLUSTED ENABLED' ADDED

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -22,13 +22,14 @@ resource "azurerm_kubernetes_cluster" "this" {
 
 
   network_profile {
-    network_plugin     = "azure"
-    load_balancer_sku  = "basic"
+    network_plugin     = "azure"  # Azure CNI (Advanced networking) is being used. FOR BASIC => network_plugin = "kubenet"
+    load_balancer_sku  = "basic"  # changing it to "standard" can =>  allow authorized IP ranges and better security.
     service_cidr       = "10.2.0.0/16"
     dns_service_ip     = "10.2.0.10"
-    docker_bridge_cidr = "172.17.0.1/16"
+    # docker_bridge_cidr = "172.17.0.1/16"   # `docker_bridge_cidr` has been deprecated 
     # We do not create an external LB. This is an internal cluster usage only.
   }
+  private_cluster_enabled = true 
 
   tags = var.tags
 }
